f387d943d9cc57ee47892a61a102f934459eb783
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-04-01 14:50:51 -0500

    🔧 Add firebase to the project

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'teach-rate-dev',
+    appId: '1:753020012195:web:40e1dfd645ddf79a48e116',
+    storageBucket: 'teach-rate-dev.appspot.com',
+    apiKey: 'AIzaSyBaP0mYYS-HOOB-oMGGDKpaSA639uGgVhc',
+    authDomain: 'teach-rate-dev.firebaseapp.com',
+    messagingSenderId: '753020012195',
+    measurementId: 'G-5YRSHZQSWV',
+  },};
