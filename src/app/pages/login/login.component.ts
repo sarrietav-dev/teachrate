d2e61fd5c0875743559464c12cc384c95439c2f5
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-04-01 22:32:38 -0500

    💄 Add the login button

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,7 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login() {
+    this.authService
+      .loginWithGoogle()
+      .subscribe(() => this.router.navigate(['feed']));
+  }
+}
