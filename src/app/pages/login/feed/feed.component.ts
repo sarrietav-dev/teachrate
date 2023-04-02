36e4e8db6fa0a07bebacf52a0fef5a78d88a9fc2
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-04-01 22:23:57 -0500

    🚧 Add routing

--- src/app/pages/login/feed/feed.component.ts
+++ src/app/pages/login/feed/feed.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-feed',
-  templateUrl: './feed.component.html'
+  templateUrl: './feed.component.html',
+  styleUrls: ['./feed.component.scss']
 })
 export class FeedComponent {
 
