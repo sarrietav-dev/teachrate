4557461950bd856d3220e13cd51349fb2b4bb17d
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-04-02 21:49:52 -0500

    🚧 Create search component

--- src/app/pages/feed/feed.module.ts
+++ src/app/pages/feed/feed.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { FeedRoutingModule } from './feed-routing.module';
 import { FeedComponent } from './feed.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
-  declarations: [FeedComponent],
+  declarations: [FeedComponent, SearchComponent],
   imports: [CommonModule, FeedRoutingModule],
 })
 export class FeedModule {}
