36a7cb7da985b73d24dcc7437a564a3682df303b
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-04-04 00:14:15 -0500

    💄 Prototype the UI

--- src/app/pages/feed/components/search/search.component.ts
+++ src/app/pages/feed/components/search/search.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-search',
+  selector: 'feed-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
 })
 export class SearchComponent {
 
