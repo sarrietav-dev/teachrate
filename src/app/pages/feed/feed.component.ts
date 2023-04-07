a96ed1315fc4b3f90a72ec8b65828421060936c6
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-04-07 12:15:34 -0500

    ♻️ Add reviews model fakes

--- src/app/pages/feed/feed.component.ts
+++ src/app/pages/feed/feed.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Review } from 'src/models/review';
+import { Review, createFakeReviews } from 'src/models/review';
 
 @Component({
   selector: 'app-feed',
@@ -7,27 +7,5 @@ import { Review } from 'src/models/review';
   styleUrls: ['./feed.component.scss'],
 })
 export class FeedComponent {
-  reviews: Review[] = [
-    {
-      id: '1',
-      content:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel ultricies lacinia, nisl nisl aliquam nisl, nec aliquam nisl nisl eu nunc. Sed euismod, nisl vel ultricies lacinia, nisl nisl aliquam nisl, nec aliquam nisl nisl eu nunc.',
-      rating: 5,
-      teacher: {
-        id: '1',
-        name: 'John Doe',
-      },
-      subject: 'Math',
-    },
-    {
-      id: '2',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      rating: 2,
-      teacher: {
-        id: '2',
-        name: 'Jane Doe',
-      },
-      subject: 'English',
-    },
-  ];
+  reviews: Review[] = createFakeReviews(10);
 }
--- src/models/review.ts
+++ src/models/review.ts
@@ -1,4 +1,5 @@
 import { Teacher } from './teacher';
+import { faker } from '@faker-js/faker';
 
 export type Review = {
   id: string;
@@ -7,3 +8,22 @@ export type Review = {
   teacher: Omit<Teacher, 'subjects'>;
   subject: string;
 };
+
+function createFakeReview(): Review {
+  return {
+    id: faker.datatype.uuid(),
+    content: faker.lorem.paragraph(),
+    rating: faker.datatype.number({ min: 1, max: 5 }),
+    teacher: {
+      id: faker.datatype.uuid(),
+      name: faker.name.fullName(),
+    },
+    subject: faker.word.noun(),
+  };
+}
+
+export function createFakeReviews(count: number = 1): Review[] {
+  return Array(count)
+    .fill(null)
+    .map(() => createFakeReview());
+}
