0cb7111637ce1eeaf45eb81df1f4b11b66c48409
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-04-06 19:46:38 -0500

    ♻️ Extract Review component

--- src/app/pages/feed/components/review/review.component.ts
+++ src/app/pages/feed/components/review/review.component.ts
@@ -1,7 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Review } from 'src/models/review';
 
 @Component({
   selector: 'feed-review',
   templateUrl: './review.component.html',
 })
-export class ReviewComponent {}
+export class ReviewComponent {
+  @Input() review!: Review;
+
+  get stars() {
+    if (!this.review.rating) return '';
+    return Array(this.review.rating).fill('⭐').join('');
+  }
+}
--- src/app/pages/feed/feed.component.ts
+++ src/app/pages/feed/feed.component.ts
@@ -1,8 +1,33 @@
 import { Component } from '@angular/core';
+import { Review } from 'src/models/review';
 
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.scss'],
 })
-export class FeedComponent {}
+export class FeedComponent {
+  reviews: Review[] = [
+    {
+      id: '1',
+      content:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel ultricies lacinia, nisl nisl aliquam nisl, nec aliquam nisl nisl eu nunc. Sed euismod, nisl vel ultricies lacinia, nisl nisl aliquam nisl, nec aliquam nisl nisl eu nunc.',
+      rating: 5,
+      teacher: {
+        id: '1',
+        name: 'John Doe',
+      },
+      subject: 'Math',
+    },
+    {
+      id: '2',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      rating: 2,
+      teacher: {
+        id: '2',
+        name: 'Jane Doe',
+      },
+      subject: 'English',
+    },
+  ];
+}
