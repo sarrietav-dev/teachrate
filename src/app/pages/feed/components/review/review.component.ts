a15ad1b64f70efbd94bbe8da40286c897b586560
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-04-07 11:21:32 -0500

    🩹 Move article styles to the host element

--- src/app/pages/feed/components/review/review.component.ts
+++ src/app/pages/feed/components/review/review.component.ts
@@ -4,6 +4,13 @@ import { Review } from 'src/models/review';
 @Component({
   selector: 'feed-review',
   templateUrl: './review.component.html',
+  styles: [
+    `
+    :host {
+      @apply rounded-md border border-black bg-white;
+    }
+  `,
+  ],
 })
 export class ReviewComponent {
   @Input() review!: Review;
