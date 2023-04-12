3fc878516896e77295f82c9e0e1c149b91e91a7f
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-04-12 04:36:30 -0500

    ✅ Add tests for auth service

--- src/app/core/services/auth/auth.service.spec.ts
+++ src/app/core/services/auth/auth.service.spec.ts
@@ -1,16 +1,49 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
+import { FirebaseAuthService } from '../firebase/firebase_auth/firebase-auth.service';
+import { of } from 'rxjs';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let fbAuthService: jasmine.SpyObj<FirebaseAuthService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const fbAuthServiceSpy = jasmine.createSpyObj('FirebaseAuthService', {
+      loginWithGoogle: of({
+        id: '123',
+        name: 'John Doe',
+        email: '',
+      }),
+    });
+
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: FirebaseAuthService,
+          useValue: fbAuthServiceSpy,
+        },
+      ],
+    });
+
     service = TestBed.inject(AuthService);
+    fbAuthService = TestBed.inject(
+      FirebaseAuthService
+    ) as jasmine.SpyObj<FirebaseAuthService>;
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return user after login', (done) => {
+    service.login().subscribe((user) => {
+      expect(user).toEqual({
+        id: '123',
+        name: 'John Doe',
+        email: '',
+      });
+      done();
+    });
   });
 });
