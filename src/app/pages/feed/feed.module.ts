b48c9a7457c040921e511f29d14f8ae04b01d4c3
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-04-15 19:00:49 -0500

    🐛 Fix IconifyAngular package not being Ivy
    Fixed by using the official iconify web component

--- src/app/pages/feed/feed.module.ts
+++ src/app/pages/feed/feed.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FeedRoutingModule } from './feed-routing.module';
 import { FeedComponent } from './feed.component';
 import { SearchComponent } from './components/search/search.component';
 import { ReviewComponent } from './components/review/review.component';
-import { IconModule } from '@visurel/iconify-angular';
 
 @NgModule({
   declarations: [FeedComponent, SearchComponent, ReviewComponent],
-  imports: [CommonModule, FeedRoutingModule, IconModule],
+  imports: [CommonModule, FeedRoutingModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class FeedModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import 'iconify-icon'
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
