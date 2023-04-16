7fd7dc616b15a76498738f138375db7f991ea2da
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-04-15 21:51:16 -0500

    🚨 Remove last ic-icon component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -38,5 +38,6 @@ import { connectFirestoreEmulator } from '@angular/fire/firestore';
   ],
   providers: [],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
